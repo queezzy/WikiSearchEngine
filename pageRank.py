964396d78a6ae3ec9289a21cff573453448dc749
ClovisG <clovis.galiez@centraliens.net>
2018-11-07 10:45:55 +0100

    Typos + page rank update

--- pageRank.py
+++ pageRank.py
@@ -18,7 +18,7 @@ def surfStep(origin, links):
 	dest = [0.0] * len(origin)
 	for idx, proba in enumerate(origin):
 		if len(links[idx]):
-			w = 1.0 / len(links[idx])
+			w = 1.0 / CHANGE_ME
 		else:
 			w = 0.0
 		for link in links[idx]:
@@ -36,18 +36,24 @@ sourceVector = [1.0/len(allPages)] * len(allPages)
 pageRanks = [1.0/len(allPages)] * len(allPages)
 delta = float("inf")
 
+# Main loop for computing the Page Rank vector
 while delta > CONVERGENCE_LIMIT:
-	print("Convergence delta:",delta,sum(pageRanks),len(pageRanks))
+	print("Convergence delta:",delta)
 	pageRanksNew = surfStep(pageRanks, linksIdx)
 	jumpProba = sum(pageRanks) - sum(pageRanksNew)
-	if jumpProba < 0: # Technical artifact due to numerical errors
+	if jumpProba < 0: # Technical artifact due to numerical float approximation
 		jumpProba = 0
-	pageRanksNew = [ pageRank + jump for pageRank,jump in zip(pageRanksNew,(p*jumpProba for p in sourceVector)) ]
-	delta = sum([abs(p - pnew) for p,pnew in zip(pageRanks,pageRanksNew)])/len(pageRanks)
+	# Add some source vector to avoid the CHANGE_ME effect
+	pageRanksNew = [ pageRank + jumpProba*jump for pageRank,jump in zip(pageRanksNew,sourceVector) ]
+	delta = CHANGE_ME
 	pageRanks = pageRanksNew
 
-bestPages = [ allPages[i] for i in numpy.argsort(pageRanks)[-20:] ]
-bestPageRanks = [ pageRanks[i] for i in numpy.argsort(pageRanks)[-20:] ]
+# For information, what are the 10 highest ranked pages:
+bestPages = reversed([ allPages[i] for i in numpy.argsort(pageRanks)[-10:] ])
+bestPageRanks = reversed([ pageRanks[i] for i in numpy.argsort(pageRanks)[-10:] ])
+for page,rank in zip(bestPages,bestPageRanks):
+	print(page,"(rank score =",rank,")")
+
 
 # Name the entries of the pageRank vector
 pageRankDict = dict()
